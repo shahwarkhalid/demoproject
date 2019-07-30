60ed2843896930b486e775c886050e6be43ff7cf
shahwarkhalid <shahwar.khalid@square63.org>
2019-07-30 15:58:44 +0500

    client: added model validation for both admin and manager

--- app/models/manager/client.rb
+++ app/models/manager/client.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Manager::Client < ApplicationRecord
+  validates_presence_of :first_name, on: :create
+  validates_presence_of :email, on: :create
+  validates_presence_of :phone, on: :create
 end
