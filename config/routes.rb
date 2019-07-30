2010ebcd964f0ec79537465d29caa82c92662284
shahwarkhalid <shahwar.khalid@square63.org>
2019-07-30 09:39:33 +0500

    Client: generated scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :clients
   root 'home#index'
   devise_for :users, controllers: { registrations: :registrations }
   namespace :admin do
