27e818daa8942d57bb2a5bc3486fef3b79e2dedc
shahwarkhalid <shahwar.khalid@square63.org>
2019-08-02 12:54:54 +0500

    payment, project: set width for container

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,7 +3,7 @@
 class Payment < ApplicationRecord
   validates_presence_of :payment_type
   validates_presence_of :title
-  validates :amount, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 10000 }
+  validates :amount, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 10001 }
 
   belongs_to :project
   belongs_to :creator, class_name: 'User'
