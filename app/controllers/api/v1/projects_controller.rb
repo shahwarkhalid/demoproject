76e55250b51adb62654a33c153953f65d3ae17cb
shahwarkhalid <shahwar.khalid@square63.org>
2019-08-19 19:23:30 +0500

    projects api

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -2,19 +2,20 @@
 
 class Api::V1::ProjectsController < ApiController
   before_action :set_project, only: %i[show update destroy]
+  before_action :set_project_for_employees, only: [:create_employees_list, :get_employees_list]
+  before_action :authorise_user, only: [:create, :update]
+  before_action :authorize_request
 
   def index
-    projects = Project.all
-    render json: projects
+    render json: Project.search_projects(params, current_user)
   end
 
   def create
     @project = Project.new(project_params)
     @project.creator_id = 10
     @project.status = 1
-    byebug
     @project.save
-    render json: @project
+    render json: @project.errors.any? ? @project.errors : @project
   end
 
   def show
@@ -23,7 +24,7 @@ class Api::V1::ProjectsController < ApiController
 
   def update
     @project.update(project_params)
-    render json: @project
+    render json: @project.errors.any? ? @project.errors : @project
   end
 
   def destroy
@@ -31,11 +32,29 @@ class Api::V1::ProjectsController < ApiController
     render json: 'project was deleted'
   end
 
+  def get_employees_list
+    render json: @project.employees.order(:id)
+  end
+
+  def create_employees_list
+    Project.add_employees_by_emails(@project, params)
+    render json: 'Employees assigned successfully'
+  end
+
   private
 
   def set_project
     @project = Project.find_by_id(params[:id])
     render json: 'record not found', status: :not_found unless @project
+  end
+
+  def set_project_for_employees
+    @project = Project.find_by_id(params[:project_id])
+    render json: 'record not found', status: :not_found unless @project
+  end
+
+  def authorise_user
+    render json: 'you are not authorised to access' if current_user.user?
   end
 
   def project_params
